317693e1ec7bd904c5f240bd360566384119fc62
David <d.li.2247@gmail.com>
2019-11-16 19:40:17 -0800

    Add repository input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            it-depends vis
-        </div>
-    );
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	);
 }
 
 export default App;
