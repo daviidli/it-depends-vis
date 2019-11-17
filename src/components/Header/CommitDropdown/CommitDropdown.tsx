3e201d6f4de8559bf5c530ee85677d2cf8ea64a3
David <d.li.2247@gmail.com>
2019-11-17 15:20:32 -0800

    Rename dropdown to commitDropdown

--- src/components/Header/CommitDropdown/CommitDropdown.tsx
+++ src/components/Header/CommitDropdown/CommitDropdown.tsx
@@ -4,7 +4,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
-import './Dropdown.scss';
+import './CommitDropdown.scss';
 
 export interface DropdownProps {
 	label: string,
