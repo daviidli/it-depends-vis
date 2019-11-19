d4c82e295ea7f110e6b646ddb8a4cf0dbc9fdb15
David <d.li.2247@gmail.com>
2019-11-19 14:26:06 -0800

    Updates

---
+++ precache-manifest.cd286c6bb005792e105633dd92e23c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedd4c10ecf672886c8594d74473b64e",
+    "url": "/it-depends-vis/index.html"
+  },
+  {
+    "revision": "1039c4b5d90604ea91e4",
+    "url": "/it-depends-vis/static/css/main.964e8c44.chunk.css"
+  },
+  {
+    "revision": "20b8a652869458ad1698",
+    "url": "/it-depends-vis/static/js/2.c27b482b.chunk.js"
+  },
+  {
+    "revision": "1039c4b5d90604ea91e4",
+    "url": "/it-depends-vis/static/js/main.9d0f02d4.chunk.js"
+  },
+  {
+    "revision": "b0564297b41803bf97ac",
+    "url": "/it-depends-vis/static/js/runtime-main.cf6ed9f9.js"
+  }
+]);
