95b6df6f121c544a698459347049b050be263015
David Li <d.li.2247@gmail.com>
2020-04-15 13:44:20 -0700

    Initial commit of v2

--- src/AppContext.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-export const SettingsContext = React.createContext<any>(null);
-export const DataContext = React.createContext<any>({});
--- src/data/types/IResponse.ts
+++
@@ -1,7 +0,0 @@
-import { DependencyType } from './DependencyType';
-
-export default interface IResponse {
-	names: string[],
-	data: number[][] | DependencyType[][][],
-	size?: number[]
-}
